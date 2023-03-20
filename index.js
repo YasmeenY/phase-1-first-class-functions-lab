b2d338c4eae623cb6b0328b674996929c5742ed0
Yasmeen Yousef <yasmeen.yousef05@gmail.com>
2023-03-20 14:32:23 -0400

    pass tests

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+const scuberNames = ['Antonia', 'Nuru', 'Amari', 'Mo']
+const returnFirstTwoDrivers = (function(scuberNames){
+    return (scuberNames.slice(0,2))
+});
+const returnLastTwoDrivers = (function(scuberNames){
+    return (scuberNames.slice(2,4))
+});
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(fare){
+    return (function(){
+        return fare * fare
+    })
+};
+
+function fareDoubler(fare){
+    return fare*2;
+};
+
+function fareTripler(fare){
+    return fare*3
+};
+function selectDifferentDrivers(scuberNames,fun){
+    return fun(scuberNames);
+};
